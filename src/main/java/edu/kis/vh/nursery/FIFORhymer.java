43d073731e9b7e26fa94c612c79b176929eee946
Michał Ogłoza <michalogloza97@gmail.com>
2019-11-26 14:13:51 +0100

    Zadanie 1.4

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 //    kombinacja alt + -> pozwala przechodzenie między plikami
     @Override
     public int countOut() {
